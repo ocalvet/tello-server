a0ee6787d4c84cbd9e1f5983d19c5af83c42ef0c
Ovidio R. Calvet <calveto@gmail.com>
2018-05-19 13:08:47 -0400

    add routes to handle video

--- app/app.go
+++ app/app.go
@@ -17,7 +17,6 @@ func CreateWorker(drone *tello.Driver, r *gin.Engine) func() {
 			AllowMethods:     []string{"*"},
 			AllowHeaders:     []string{"*"},
 		}))
-		r.Use()
 		r.GET("/tello/takeoff", func(c *gin.Context) {
 			drone.TakeOff()
 			c.JSON(http.StatusOK, gin.H{
@@ -33,28 +32,28 @@ func CreateWorker(drone *tello.Driver, r *gin.Engine) func() {
 		})
 
 		r.GET("/tello/left", func(c *gin.Context) {
-			drone.Left(1)
+			drone.Left(10)
 			c.JSON(http.StatusOK, gin.H{
 				"success": true,
 			})
 		})
 
 		r.GET("/tello/right", func(c *gin.Context) {
-			drone.Right(1)
+			drone.Right(10)
 			c.JSON(http.StatusOK, gin.H{
 				"success": true,
 			})
 		})
 
 		r.GET("/tello/forward", func(c *gin.Context) {
-			drone.Forward(1)
+			drone.Forward(10)
 			c.JSON(http.StatusOK, gin.H{
 				"success": true,
 			})
 		})
 
 		r.GET("/tello/backward", func(c *gin.Context) {
-			drone.Backward(1)
+			drone.Backward(10)
 			c.JSON(http.StatusOK, gin.H{
 				"success": true,
 			})
@@ -82,6 +81,25 @@ func CreateWorker(drone *tello.Driver, r *gin.Engine) func() {
 		})
 
 		r.GET("/tello/flip/back", func(c *gin.Context) {
+			drone.BackFlip()
+			c.JSON(http.StatusOK, gin.H{
+				"success": true,
+			})
+		})
+
+		r.GET("/tello/startfeed", func(c *gin.Context) {
+			// drone.StartVideo()
+			// drone.SetVideoEncoderRate(4)
+			// gobot.Every(100*time.Millisecond, func() {
+			// 				drone.StartVideo()
+			// })
+			drone.FrontFlip()
+			c.JSON(http.StatusOK, gin.H{
+				"success": true,
+			})
+		})
+
+		r.GET("/tello/endfeed", func(c *gin.Context) {
 			drone.BackFlip()
 			c.JSON(http.StatusOK, gin.H{
 				"success": true,
